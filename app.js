7c94d3fa561fe41595e13a19b7d8c25c7752f103
juangc98 <juangc_98@hotmail.com>
2020-03-19 17:09:12 -0300

    Funcionalidades mas prolijas, faltan mejoras.

--- app.js
+++ app.js
@@ -5,41 +5,43 @@ let accion = process.argv[2];
 
 
 switch(accion) {
+
     case 'listar':
         console.log('Listado de tareas');
+        console.log('-----------------');
+        let estado = process.argv[3];
         let tareas = archivoTareas.leerJSON();
-        tareas.forEach(function(tarea, i ) {
-            console.log(i + '. ' + tarea.titulo + ' (' + tarea.estado + ')' + ': ' + tarea.cuerpo)
-        });
-        console.log()
+        
+        if (estado == undefined){
+           tareas.forEach(function (tarea, i) {
+                console.log();
+                console.log(i + '. ' + tarea.titulo + ' (' + tarea.estado + ')' + ': ' + tarea.cuerpo);
+                console.log();
+            })} else {
+                let tareasPorEstado = tareas.filter(function(tarea) {
+                    return tarea.estado === estado;
+                });
+                tareasPorEstado.forEach(e => {
+                    console.log(e);
+            })
+          };
+        console.log();
         break;
 
     case 'agregar': 
       let nuevaTarea = {
-        titulo: "Prueba 1", 
-        estado: "en progreso", 
+        titulo: "Prueba 5", 
+        estado: "pendiente", 
         cuerpo: null
       };
-        archivoTareas.escribirJSON2(nuevaTarea, null, " ");  
+        archivoTareas.crear(nuevaTarea);  
         console.log("Nueva tarea agregada!");
         break;
         
-    case 'guardar': 
-        let listaTareasObj = archivoTareas.leerJSON();
-        let tareaAgregada = archivoTareas.leerJSON2();
-        listaTareasObj.push(tareaAgregada);
-        
-        //console.log(listaTareasObj);  
-        archivoTareas.escribirJSON(listaTareasObj);
-
-        console.log("Guardada!");
-        break; 
-
-    case 'filtrar':
+    /*case 'filtrar':
         let estado = process.argv[3];
         let todasTareas = archivoTareas.leerJSON();
         //let todasTareasObj = JSON.parse(todasTareas)
-
          let tareasPorEstado = todasTareas.filter(function(tarea){
             return tarea.estado === estado ; 
          });
@@ -48,6 +50,37 @@ switch(accion) {
              });
         // console.log(tareasPorEstado);
         break;
+    */
+
+    case 'eliminar':
+        let indice = process.argv[3];
+        //let tareaOut
+        archivoTareas.eliminar(indice);
+        console.log("La tarea " + indice + " fue eliminada.")
+        //console.log(tareaEliminada)
+        //console.log(tareaOut);
+        break;
+
+    case 'actualizar':
+        let index = process.argv[3];
+        let nuevoEstado = process.argv[4];
+        archivoTareas.actualizar(index, nuevoEstado);
+        let listadoTareas = archivoTareas.leerJSON();
+        console.log("Estado actualizado " + listadoTareas[index]);
+        break;
+
+    
+    //case 'ordenar':
+
+    //case 'guardar': 
+    //    let listaTareasObj = archivoTareas.leerJSON();
+    //    let tareaAgregada = archivoTareas.leerJSON2();
+    //    listaTareasObj.push(tareaAgregada);
+    //    console.log(listaTareasObj);  
+    //    archivoTareas.escribirJSON(listaTareasObj);
+    //    console.log("Guardada!");
+    //    break; 
+
 
     case undefined:
         console.log('Tenés que pasarme una acción');
--- tareas.js
+++ tareas.js
@@ -4,23 +4,62 @@ let archivoTareas = {
 
     archivo: 'listaTareas.json',
     nuevoArchivo: 'nuevaTarea.json',
+    
+
 
     leerJSON: function () {
+        if (fs.existsSync(this.archivo)){
         return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
+        } else { 
+        return console.log('No hay tareas :)');
+        }
     },
 
-    leerJSON2: function () {
+    escribirJSON: function (datos) {
+        return fs.writeFileSync(this.archivo, JSON.stringify(datos, null, ' '));
+    },
+
+    crear : function(nuevaTarea) {
+        let listaDeTareas = this.leerJSON();
+        listaDeTareas.push(nuevaTarea);
+        this.escribirJSON(listaDeTareas);
+        //fs.writeFileSync(this.archivo, JSON.stringify(listaDeTareas, null, ' '));
+    },
+
+    actualizar : function (indice, nuevoEstado) {
+        let listaDeTareas = this.leerJSON();
+        listaDeTareas.forEach(function(tarea, i){
+            if (indice == i){
+                tarea.estado = nuevoEstado;
+            }
+        })
+      this.escribirJSON(listaDeTareas);
+      //fs.writeFileSync(this.archivo, JSON.stringify(listaDeTareas, null, ' '))
+    },
+
+    eliminar: function (indice) {
+        let listaTareas = this.leerJSON();
+        listaTareas.map(function(x, i) {
+            if (indice === i) {
+                listaTareas.splice(i, 1);
+            }
+            return listaTareas
+        })
+        return this.escribirJSON(listaTareas);
+     },
+    
+
+        //var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];for( var i = 0; i < arr.length; i++){ if ( arr[i] === 5) { arr.splice(i, 1); }}//=> [1, 2, 3, 4, 6, 7, 8, 9, 0]
+
+    /*leerJSON2: function () {
         return JSON.parse(fs.readFileSync(this.nuevoArchivo, 'utf-8'));
     },
 
-    escribirJSON: function (a) {
-        return fs.writeFileSync(this.archivo, JSON.stringify(a));
-    },
-
+  
     escribirJSON2: function (nuevaTarea) {
-           let tareaArray = fs.writeFileSync(this.nuevoArchivo, JSON.stringify(nuevaTarea));
+           let tareaArray = fs.writeFileSync(this.nuevoArchivo, JSON.stringify(nuevaTarea, null, ' '));
         },
-
+    */
     
 }
 
